ea78246a08a9ec365d630b3e4f1148d55407c809
Filip Zadražil <filipzadrazil@seznam.cz>
2021-01-05 02:43:52 +0100

    přidání komentářů

--- ukol_2_skript.py
+++ ukol_2_skript.py
@@ -2,6 +2,7 @@ import json, pyproj, math, statistics
 from pyproj import Transformer
 from math import hypot 
 
+#Funkce, která otevírá geojson soubory kontejnerů a ošetřuje nekorektní vstupy 
 def containers_file_open(container_file):
     try:
         with open(container_file, encoding = 'utf-8') as f:
@@ -15,9 +16,11 @@ def containers_file_open(container_file):
         exit()
     except PermissionError:
         print(f"Soubor {container_file} je nepřístupný.")
+        exit()
     
     return features
 
+#Funkce, která vyfiltruje jen ty kontejnery, které jsou volně přístupné 
 def container_access_filter(features):
     containers_coords_sjtsk = []
     for feature in features:
@@ -26,8 +29,9 @@ def container_access_filter(features):
             containers_coords_sjtsk.append(souradnice)
     return containers_coords_sjtsk
 
-def address_file_open(address_file):
-    try:
+#Funkce, která otevírá geojson soubory adres a ošetřuje nekorektní vstupy 
+#def address_file_open(address_file):
+    """try:
         with open(address_file, encoding = 'utf-8') as f:
             address_input = json.load(f)
             addresses = address_input['features']
@@ -39,9 +43,11 @@ def address_file_open(address_file):
         exit()
     except PermissionError:
         print(f"Soubor {address_file} je nepřístupný.")
+        exit()
     
-    return addresses
+    return addresses"""
 
+#Funkce, která převede souřadnice adres z WGS84 do S-JTSK a vrátí list slovníků obsahujících adresu a souřadnice adresního bodu
 def address_points(addresses):
     positions = []
     transformer = Transformer.from_crs(4326, 5514, always_xy=True)
@@ -52,8 +58,10 @@ def address_points(addresses):
         positions.append(records)
     return positions
 
+#Funkce, která vypočítá nejkratší vzdálenosti ke kontejneru pro každý adresního bod
+#Zároveň funkce vybere největší hodnotu z nejkratších vzdáleností a vrátí ji společně s adresou, pro níž nejvyšší hodnota platí 
 def address_point_container_distance(positions, containers_coords_sjtsk):
-    farthest_distance_address = 'Filip'
+    farthest_distance_address = 'Brno'
     distances = []
     farthest_value = 0
     for adresa in positions:
@@ -70,15 +78,18 @@ def address_point_container_distance(positions, containers_coords_sjtsk):
             farthest_distance_address = adresa['adresa']
     return farthest_value, farthest_distance_address, distances
 
+#Spuštění výše vypsaných funkcí
 features = containers_file_open("soubor_kontejnery.geojson")
 addresses = address_file_open("soubor_adresy.geojson")  
 containers_coords_sjtsk = container_access_filter(features) 
 positions = address_points(addresses) 
 farthest_value, farthest_distance_address, distances = address_point_container_distance(positions, containers_coords_sjtsk)
-  
+
+#Výpočet průměru a mediánu nejkratších vzdáleností ke kontejnerům  
 mean = statistics.mean(distances)
 median = statistics.median(distances)
 
+#Výstup programu
 print("Celkem načteno", len(positions), "adresních bodů")
 print("Celkem načteno", len(containers_coords_sjtsk), "kontejnerů na tříděný odpad")
 print("Průměrná vzdálenost z adresního bodu ke kontejneru je:", f"{mean:.0f}", "m")
